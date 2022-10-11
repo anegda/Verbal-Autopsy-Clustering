151d4b08b50856ed348d4f046a6e1485971a3080
mmarcos-ubuntu <mmerino08p@gmail.com>
2022-10-11 11:29:14 +0200

    vectorizacion por topic modeling

--- preproceso.py
+++ preproceso.py
@@ -1,6 +1,6 @@
 import json
 import re
-
+import pickle
 import nltk
 import pandas as pd
 from nltk import WordNetLemmatizer
@@ -10,11 +10,13 @@ from nltk.tokenize import ToktokTokenizer
 import csv
 from sklearn.feature_extraction.text import TfidfVectorizer
 from gensim.corpora import Dictionary
+from gensim.models import LdaModel
 from sklearn.decomposition import LatentDirichletAllocation
 import random
 import numpy as np
 import matplotlib.pyplot as plt
 
+nltk.download("stopwords")
 
 STOPWORDS = set(stopwords.words("english"))
 wnl = WordNetLemmatizer()
@@ -54,6 +56,28 @@ def display_topics(H, W, feature_names, documents, no_top_words, no_top_document
         for doc_index in top_doc_indices:
             print(documents[doc_index])
 
+def topicosReview(review):
+    # Cargo el modelo lda
+    file = open("./modelos/lda.sav", "rb")
+    lda = pickle.load(file)
+    file.close()
+
+    bow_review = cuerpo[indice_review]
+    distribucion_review = lda[bow_review]
+
+    # Indices de los topicos mas significativos
+    dist_indices = [topico[0] for topico in lda[bow_review]]
+    # Contribución de los topicos mas significativos
+    dist_contrib = [topico[1] for topico in lda[bow_review]]
+
+    distribucion_topicos = pd.DataFrame({'Topico': dist_indices,
+                                         'Contribucion': dist_contrib})
+
+    print(distribucion_topicos.head())
+
+    return dist_contrib
+
+
 # ---> Parte 1: https://elmundodelosdatos.com/topic-modeling-gensim-fundamentos-preprocesamiento-textos/
 #ruta = str(input("Introduce el path relativo (EJ: ./datasets/nombre.csv) :"))
 ruta = "./datasets/train.csv"
@@ -86,8 +110,8 @@ print(f'Número de tokens: {len(diccionario)}') #mostrar el numero se palabras
 diccionario.filter_extremes(no_below=2, no_above = 0.8)
 print(f'Número de tokens: {len(diccionario)}')
 
-# Creamos el corpus (por cada roken en el df) QUE ES UN ARRAY BOW
-corpus = [diccionario.doc2bow(review) for review in df.Tokens]
+# Creamos el corpus (por cada token en el df) QUE ES UN ARRAY BOW
+cuerpo = [diccionario.doc2bow(review) for review in df.Tokens]
 
 # BOW de una review
 # print(corpus[5])
@@ -98,12 +122,13 @@ tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, stop_words="english")
 tf = tf_vectorizer.fit_transform(documents.values.astype(str))
 tf_feature_names = tf_vectorizer.get_feature_names()
 
-lda = LatentDirichletAllocation(corpus=corpus, id2word=diccionario,
+lda = LdaModel(corpus=cuerpo, id2word=diccionario,
                num_topics=5, random_state=42,
                chunksize=1000, passes=1,
                alpha=2, eta=2)
 
-# Imprimimos los topicos creados con las 5 palabras que más contribuyen a ese tópico y sus pesos
+
+'''# Imprimimos los topicos creados con las 5 palabras que más contribuyen a ese tópico y sus pesos
 lda_W = lda.transform(tf)
 lda_H = lda.components_
 
@@ -117,59 +142,20 @@ for index, component in enumerate(lda_H):
     print("Topic "+str(index)+": ",top_terms_list)
 lda_H=lda.components_ /lda.components_.sum(axis=1)[:, np.newaxis]  #esto cambia
 print("LDA Topics")
-display_topics(lda_H, lda_W, tf_feature_names, documents, 10, 10)
+display_topics(lda_H, lda_W, tf_feature_names, documents, 10, 10)'''
 
 # Aqui imprimimos una review aleatoria para comprobar la eficacia de nuestro modelo!
 indice_review = random.randint(0,len(df))
 review = df.iloc[indice_review]
 
-import pickle
-pickle.dump(lda, open("./modelos/lda.sav", "wb"))
-
 print("***********************")
 print("\nReview: " + review[0] + "\n")
 print("***********************")
 
-'''
-# Obtenemos el BOW de la review
-# Obtenemos la distribucion de topicos
-bow_review = corpus[indice_review]
-distribucion_review = lda[bow_review]
+# Guardo el modelo
+file = open("./modelos/lda.sav", "wb")
+pickle.dump(lda, file)
+file.close()
 
-# Indices de los topicos mas significativos
-dist_indices = [topico[0] for topico in lda[bow_review]]
-# Contribución de los topicos mas significativos
-dist_contrib = [topico[1] for topico in lda[bow_review]]
-
-# Representacion grafica de los topicos mas significativos
-distribucion_topicos = pd.DataFrame({'Topico':dist_indices,
-                                     'Contribucion':dist_contrib })
-distribucion_topicos.sort_values('Contribucion',
-                                 ascending=False, inplace=True)
-ax = distribucion_topicos.plot.bar(y='Contribucion',x='Topico',
-                                   rot=0, color="orange",
-                                   title = 'Tópicos mas importantes'
-                                   'de review ' + str(indice_review))
-
-# Imprimimos las palabras mas significativas de los topicos
-cabeceras = ["num_topics", "alpha", "beta"]
-nombreTXT = "Gnsm-" + "nTopics_" + str(lda.num_topics) + "-alpha_" + str(lda.alpha[0]).split(".")[0] + "-beta_" + str(lda.eta[0]).split(".")[0] + ".txt"
-print(nombreTXT)
-archivo = open(nombreTXT, "w")
-writer = csv.writer(archivo)
-print("PREPARANDO ARCHIVO .TXT PARA VOLCAR DISTRIBUCION PALABRAS EN LOS TOPICOS...")
-
-
-for ind, topico in distribucion_topicos.iterrows():
-
-    print("*** Tópico: " + str(int(topico.Topico)) + " ***")
-    writer.writerow("*** Tópico: " + str(int(topico.Topico)) + " ***\n")
-
-    palabras = [palabra[0] for palabra in lda.show_topic(
-        topicid=int(topico.Topico))]
-    palabras = ', '.join(palabras)
-    print(palabras, "\n")
-    writer.writerow(str(palabras)+ "\n\n\n")
-
-archivo.close()
-'''
+# Para esta review random sacamos el array de contribuciones de cada topico
+topicosReview(review)
