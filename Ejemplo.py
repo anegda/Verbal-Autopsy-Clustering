6cabbd521e7d7018d3295fd9fe65c3d155efba62
anegda <anegdaldama@gmail.com>
2022-10-26 11:18:15 +0200

    Más cosas para la documentación

--- Ejemplo.py
+++ Ejemplo.py
@@ -10,14 +10,14 @@ centers = [(0, 4), (5, 5) , (8,2)]
 cluster_std = [1.2, 1, 1.1]
 X, y= make_blobs(n_samples=200, cluster_std=cluster_std, centers=centers, n_features=2, random_state=1)
 #radius of the circle defined as 0.6
-eps = 0.6
+eps = 0.7
 #minimum neighbouring points set to 3
 minPts = 3
 
 data = pd.DataFrame(X, columns = ["X", "Y"] )
 
 dbscan = DBSCAN.DBScan()
-clusters = dbscan.fit(eps, minPts, data)
+clusters = dbscan.fitEjemplo(eps, minPts, data)
 idx , cluster = list(zip(*clusters))
 cluster_df = pd.DataFrame(clusters, columns = ["idx", "cluster"])
 
@@ -29,5 +29,5 @@ plt.legend([f"Cluster {clust}" for clust in np.unique(cluster)], loc ="lower rig
 plt.title('Clustered Data')
 plt.xlabel('X')
 plt.ylabel('Y')
-plt.show()
-plt.savefig('Imagenes/EjemploClustering.png')
+#plt.show()
+plt.savefig('Imagenes/makeblobs.png')
